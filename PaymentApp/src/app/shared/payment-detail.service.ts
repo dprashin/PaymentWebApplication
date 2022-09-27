8b85a4d459cc6aa0bcafd7964d7b4c4bf63d2b5b
Devendra Prashin <dp@intect.io>
2022-09-27 23:05:43 +0200

    Fetch and display card list

--- PaymentApp/src/app/shared/payment-detail.service.ts
+++ PaymentApp/src/app/shared/payment-detail.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
 import { PaymentDetail } from './payment-detail.model';
 
-import {HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaymentDetailService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
+  formData: PaymentDetail = new PaymentDetail();
 
-  formData:PaymentDetail = new PaymentDetail();
+  list: PaymentDetail[];
 
-  readonly baseUrl = "http://localhost:5197/api/PaymentDetails";
-  postPaymentDetail(){
-    return this.http.post(this.baseUrl, this.formData)
+  readonly baseUrl = 'http://localhost:5197/api/PaymentDetails';
+
+  postPaymentDetail() {
+    return this.http.post(this.baseUrl, this.formData);
+  }
+
+  refreshList() {
+    this.http.get(this.baseUrl)
+    .toPromise()
+    .then(res => this.list = res as PaymentDetail[]);
   }
 }
